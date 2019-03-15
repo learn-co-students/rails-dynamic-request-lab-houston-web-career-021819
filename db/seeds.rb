82d702da77684ca04fb28f42d487a4b57fd61970
sm00th <ezzybrzy@sm00ths-MacBook-Air.local>
2019-03-14 20:33:42 -0500

    dome

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+swag = Student.create(first_name: 'Swag', last_name: 'Mcjagger')
